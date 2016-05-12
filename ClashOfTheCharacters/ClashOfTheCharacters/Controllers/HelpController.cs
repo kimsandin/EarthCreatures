0e898e4160a4fa6eab71134c3895b0fed9ad7325
Kim <kim_sandin@yahoo.se>
2016-05-12 20:03:21 +0200

    Senaste publicerade version med fungerande webmail-form m.m.

--- ClashOfTheCharacters/ClashOfTheCharacters/Controllers/HelpController.cs
+++ ClashOfTheCharacters/ClashOfTheCharacters/Controllers/HelpController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ClashOfTheCharacters.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace ClashOfTheCharacters.Controllers
@@ -22,6 +24,57 @@ namespace ClashOfTheCharacters.Controllers
         public ActionResult Gameguide()
         {
             return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult ContactForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult ContactForm(ContactRequest contactRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                var customerName = contactRequest.Name;
+                var customerEmail = contactRequest.Email;
+                var customerRequest = contactRequest.SupportRequest;
+                var customerPhone = contactRequest.Phone;
+                var supportEmail = "creatures@cimoco.se";
+                //TODO: Email question to support
+                try
+                {
+                    WebMail.SmtpServer = "smtp01.binero.se";
+                    WebMail.SmtpPort = 587;
+                    WebMail.SmtpUseDefaultCredentials = false;
+                    WebMail.EnableSsl = true;
+                    WebMail.UserName = "creatures@cimoco.se";
+                    WebMail.Password = "123456";
+                    WebMail.From = supportEmail;
+                    WebMail.Send(to: supportEmail, subject: "Help request from - " + customerName,
+                        body: "Support request from: " + customerName + ", \nPhonenumber: " + customerPhone + ", \nEmail: " + customerEmail + ", \nQuestion: " + customerRequest
+
+
+                    );
+                    return View("Thanks", contactRequest);
+                }
+                catch (Exception)//(NullReferenceException ex)
+                {
+                    //ViewBag.ErrorMessage = ex.Message;
+                    return View("SendEmailError");
+                }
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+
         }
     }
 }
---
+++ ClashOfTheCharacters/ClashOfTheCharacters/Models/ContactRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClashOfTheCharacters.Models
+{
+    public class ContactRequest
+    {
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email adress")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email adress")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter your question")]
+        public string SupportRequest { get; set; }
+
+
+    }
+}
